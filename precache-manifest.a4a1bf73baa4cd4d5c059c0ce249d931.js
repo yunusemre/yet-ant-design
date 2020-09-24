31a4354961f577ee9d4883ad663b64d625536b04
Yunus Emre Tatar <emre.tatar@sestek.com>
2020-09-24 13:49:29 +0300

    Updates

---
+++ precache-manifest.a4a1bf73baa4cd4d5c059c0ce249d931.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03d6e35b81c6460d739652c219601bd",
+    "url": "/ant-design/index.html"
+  },
+  {
+    "revision": "446fc8e369db0c51a60a",
+    "url": "/ant-design/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "4c43ee606a2a7201e86e",
+    "url": "/ant-design/static/css/main.9e02e47a.chunk.css"
+  },
+  {
+    "revision": "446fc8e369db0c51a60a",
+    "url": "/ant-design/static/js/2.510b3fd6.chunk.js"
+  },
+  {
+    "revision": "4480c0be8de8e8ccee43f73c5c7867ce",
+    "url": "/ant-design/static/js/2.510b3fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c43ee606a2a7201e86e",
+    "url": "/ant-design/static/js/main.7c1f9db7.chunk.js"
+  },
+  {
+    "revision": "685a6d4e6761ed91460e",
+    "url": "/ant-design/static/js/runtime-main.7c977740.js"
+  },
+  {
+    "revision": "b5c11bc41529f1bf1784522258276081",
+    "url": "/ant-design/static/media/arrow.b5c11bc4.svg"
+  }
+]);
