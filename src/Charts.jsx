6b33b67741f9a6fdbdb11b4252c59be870f58760
Yunus Emre Tatar <emre.tatar@sestek.com>
2020-09-24 16:17:04 +0300

    change width

--- src/Charts.jsx
+++ src/Charts.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BarChart, BarChartOther } from './components/charts';
+import React from "react";
+import { BarChart, BarChartOther } from "./components/charts";
 
 const Charts = () => (
   <div className="charts">
-    <BarChart width="30%" margin="10px" />
-    <BarChartOther width="30%" margin="10px" />
+    <BarChart width="50%" margin="10px" />
+    <BarChartOther width="50%" margin="10px" />
   </div>
 );
 
